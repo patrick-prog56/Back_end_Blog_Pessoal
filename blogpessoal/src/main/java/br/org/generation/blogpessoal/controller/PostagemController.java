4c467776a625b196bb333e1e0c54607c5577586b
Patrick <patricksantana56@gmail.com>
2021-06-07 21:13:13 -0300

    Adicionando security do Blog Pessoal

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
@@ -20,27 +20,27 @@ import br.org.generation.blogpessoal.model.Postagem;
 import br.org.generation.blogpessoal.repository.PostagemRepository;
 
 
-//=============== Find All ============
-@RestController //indica que essa classe Ã© controller
-@RequestMapping("/postagens") //indica o endpoint que vai ser acessado essa classe, ou seja, define a rota, a URL
-@CrossOrigin("*") //diz que essa classe vai aceitar requisiÃ§Ãµes de qualquer origem, como react e etc.
+
+@RestController //indica que essa classe Ã© controller = indica que essa classe vai receber as requisições Http e respondê-las de acordo com os verbos, endereço(end point) e messsage body  
+@RequestMapping("/postagens") //indica o endpoint que vai ser acessado o inicio da classe, ou seja, define a rota, a URL
+@CrossOrigin("*") // sem isso o front end não funciona muito bem = diz que essa classe vai aceitar requisiÃ§Ãµes de qualquer origem do front end, como react e etc.
 public class PostagemController {
 	
 	
 	@Autowired //Atribui todas as permissÃµes e responsabilidades para instanciar as coisas para o Spring
-	private PostagemRepository repository;
+	private PostagemRepository repository; //da aceso ao banco de dados.
 
-
+//=======================Find All==========================
 	@GetMapping //sempre que vier requisiÃ§Ãµes externas atravÃ©s da URL, se o mÃ©todo for GET, irÃ¡ disparar este mÃ©todo abaixo
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 		
 	}
-	//==================Fin By Id=================
+	//==================Find By Id=================
 	@GetMapping("/{id}") //informar qual vai ser o parametro enviado para quem estÃ¡ fazendo a requisiÃ§Ã£o
 public ResponseEntity<Postagem> GetById(@PathVariable long id){//captura valor que vem pela URL
-		return repository.findById (id)
-				.map(resp -> ResponseEntity.ok(resp)) //retorna ok se o objeto for encontrado
+		return repository.findById (id) //executa o metodo
+				.map(resp -> ResponseEntity.ok(resp)) //retorna ok se o objeto for encontrado,  funciona como IF
 				.orElse(ResponseEntity.notFound().build());//retorna not found se o objeto nÃ£o for encontrado se tiver erro na requisiÃ§Ã£o ou se a resposta vier nula.
 		
 		
@@ -48,7 +48,7 @@ public ResponseEntity<Postagem> GetById(@PathVariable long id){//captura valor q
 	
 	//==============Find By Titulo =====================================================
 	
-	@GetMapping("/titulo/{titulo}")
+	@GetMapping("/titulo/{titulo}")//o que está entre chaves é uma variável e é o que eu digitar
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
@@ -66,7 +66,7 @@ public ResponseEntity<Postagem> GetById(@PathVariable long id){//captura valor q
 		
 	}
 	
-	//============================controller================
+	//============================controller Delete ================
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
